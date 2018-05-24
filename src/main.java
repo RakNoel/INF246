62bd44811b78c51058f96b6b0bd3ba46948f25ae
Oskar Leirvåg <oskar.leir@gmail.com>
2018-05-24 21:36:24 +0200

    starting off

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
+
+/**
+ * TODO: Describe class
+ *
+ * @author RakNoel
+ * @version 1.0
+ * @since 24.05.18
+ */
+public class main {
+
+}
